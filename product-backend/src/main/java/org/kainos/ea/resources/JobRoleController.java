bf9cc2427b6d1256043d5f9052dea6653d75f71d
Wojciech Jakimczyk <multipanj@gmail.com>
2023-08-16 16:04:06 +0200

    fix:Capability

--- product-backend/src/main/java/org/kainos/ea/resources/JobRoleController.java
+++ product-backend/src/main/java/org/kainos/ea/resources/JobRoleController.java
@@ -1,11 +1,16 @@
 package org.kainos.ea.resources;
 
 import io.swagger.annotations.Api;
+import org.kainos.ea.api.CapabilityService;
 import org.kainos.ea.api.JobRoleService;
 import org.kainos.ea.cli.AddJobRole;
+import org.kainos.ea.cli.Capability;
+import org.kainos.ea.client.FailedToCreateCapabilityException;
 import org.kainos.ea.client.FailedToCreateJobRoleException;
 import org.kainos.ea.client.FailedToGetJobRolesException;
+import org.kainos.ea.db.CapabilityDao;
 import org.kainos.ea.db.JobRoleDao;
+import org.kainos.ea.exception.NameTooShortException;
 
 import javax.validation.Valid;
 import javax.ws.rs.GET;
@@ -14,6 +19,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
 import java.sql.SQLException;
 import java.util.OptionalInt;
 
@@ -21,6 +28,7 @@ import java.util.OptionalInt;
 @Path("/api")
 public class JobRoleController {
     private JobRoleService jobRoleService = new JobRoleService(new JobRoleDao());
+    private CapabilityService capabilityService = new CapabilityService(new CapabilityDao());
 
     @GET
     @Path("/job-roles")
@@ -51,6 +59,23 @@ public class JobRoleController {
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Database error while creating job role")
                     .build();
+        }
+    }
+
+    @POST
+    @Path("/admin/capabilities")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addCapability(Capability capability) throws NameTooShortException, SQLException, FailedToCreateCapabilityException {
+
+        try {
+            int CapabilityId = capabilityService.addCapability(capability);
+            URI location = UriBuilder.fromPath("/admin/band/" + CapabilityId).build();
+
+            return Response.created(location).build();
+        } catch (FailedToCreateCapabilityException e) {
+            System.err.println(e.getMessage());
+
+            return Response.serverError().build();
         }
     }
 }
