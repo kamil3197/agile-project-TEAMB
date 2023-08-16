f3a7479300a78a1da67290cf3e202340252a4b2b
Oleksandr Gneushev <oleksandr.gneushev@kainos.com>
2023-08-16 12:43:28 +0200

    add frontend fetch config

---
+++ product-web-app/controller/capabilityController.ts
@@ -0,0 +1,19 @@
+import { Application, Request, Response } from 'express';
+import CapabilityService from '../service/capabilityService.js';
+import Capability from '../model/capability.js';
+
+export default class CapabilityController {
+  private capabilityServiceClass = new CapabilityService();
+
+  init(app: Application): void {
+    app.get('/capability-leads', async (req: Request, res: Response) => {
+      const capabilityService = this.capabilityServiceClass;
+      try {
+        const capabilities: Capability[] = await capabilityService.getAllCapabilities();
+        res.render('capability-leads', { capabilities, title: 'Capabilities' });
+      } catch (e) {
+        res.status(500).send('Error - failed to get capabilities');
+      }
+    });
+  }
+}
