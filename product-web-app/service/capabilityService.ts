2718613609c74767dbcb2672cf877fc70b12c0b5
Oleksandr Gneushev <oleksandr.gneushev@kainos.com>
2023-08-17 09:48:32 +0200

    fix lint

--- product-web-app/service/capabilityService.ts
+++ product-web-app/service/capabilityService.ts
@@ -1,25 +1,6 @@
 import axios from 'axios';
 import Capability from '../model/capability.js';
 
-const CapabilityExample: Capability[] = [
-  {
-    capabilityId: 1,
-    capabilityName: 'New capability',
-    leadName: 'Sweet Kitty',
-    leadPhoto:
-      'https://www.shutterstock.com/shutterstock/photos/1918637450/display_1500/stock-photo-kiev-ukraine-april-hello-kitty-on-paper-hello-kitty-is-a-cartoon-character-produced-1918637450.jpg',
-    leadMessage: 'Nice job!',
-  },
-  {
-    capabilityId: 2,
-    capabilityName: 'New capability',
-    leadName: 'Sweet Kitty',
-    leadPhoto:
-      'https://www.shutterstock.com/shutterstock/photos/1918637450/display_1500/stock-photo-kiev-ukraine-april-hello-kitty-on-paper-hello-kitty-is-a-cartoon-character-produced-1918637450.jpg',
-    leadMessage: 'Nice job!',
-  },
-];
-
 class CapabilityService {
   private API_URL: string;
 
@@ -35,10 +16,6 @@ class CapabilityService {
       throw new Error('Error - failed to get capabilities');
     }
   }
-
-  // async getAllCapabilities(): Promise<Capability[]> {
-  //   return CapabilityExample;
-  // }
 }
 
 export default CapabilityService;
