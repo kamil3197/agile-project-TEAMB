a7556d3129606f60fd390eb2808bd1653f2040c1
Oleksandr Gneushev <oleksandr.gneushev@kainos.com>
2023-08-17 11:07:49 +0200

    requested changes provided

--- product-web-app/controller/capabilityController.ts
+++ product-web-app/controller/capabilityController.ts
@@ -3,13 +3,12 @@ import CapabilityService from '../service/capabilityService.js';
 import Capability from '../model/capability.js';
 
 export default class CapabilityController {
-  private capabilityServiceClass = new CapabilityService();
+  private capabilityService = new CapabilityService();
 
   init(app: Application): void {
     app.get('/capability-leads', async (req: Request, res: Response) => {
-      const capabilityService = this.capabilityServiceClass;
       try {
-        const capabilities: Capability[] = await capabilityService.getAllCapabilities();
+        const capabilities: Capability[] = await this.capabilityService.getAllCapabilities();
         res.render('capability-leads', { capabilities, title: 'View capabilities' });
       } catch (e) {
         res.status(500).send('Error - failed to get capabilities');
