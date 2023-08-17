8836db85d552fee5075f77a957808c30e7987603
Jakub Pilarski <jakub.pilarski@kainos.com>
2023-08-17 12:54:04 +0200

    check fix

--- product-web-app/tests/AuthMiddleware.test.ts
+++ product-web-app/tests/AuthMiddleware.test.ts
@@ -1,28 +1,24 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
-import AuthMiddleware from '../middleware/authMiddleware.js';
 import app from '../app.js';
-import Login from '../model/login.js';
 
 chai.use(chaiHttp);
 
-
 describe('AuthMiddleware', () => {
-
   it('should redirect to /auth/login if no token', async () => {
     const response = await chai.request(app).get('/some/path');
-    console.log(response.redirects[0].slice(-11))
+
     expect(response).to.redirect;
     expect(response.redirects[0].slice(-11)).to.be.equal('/auth/login');
   });
 
-//   it('should pass through if token is valid', async () => {
-//     const response = await chai
-//         .request(app)
-//         .get('/')
-//         .set('Cookie', 'token=myAuthToken'); 
-    
-//         expect(response).to.have.status(200);
-//     expect(response.text).to.equal('Authenticated Route');
-//   });
+  //   it('should pass through if token is valid', async () => {
+  //     const response = await chai
+  //         .request(app)
+  //         .get('/')
+  //         .set('Cookie', 'token=myAuthToken');
+
+  //         expect(response).to.have.status(200);
+  //     expect(response.text).to.equal('Authenticated Route');
+  //   });
 });
