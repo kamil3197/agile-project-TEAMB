419e67ddf78fa6f1d9f9ba20e8d36e35e6d90397
Oleksandr Gneushev <oleksandr.gneushev@kainos.com>
2023-08-17 10:51:04 +0200

    fix dependencies

--- product-backend/src/main/java/org/kainos/ea/resources/CapabilityController.java
+++ product-backend/src/main/java/org/kainos/ea/resources/CapabilityController.java
@@ -1,23 +1,15 @@
 package org.kainos.ea.resources;
 
 import io.swagger.annotations.Api;
-import org.glassfish.jersey.server.Uri;
 import org.kainos.ea.api.CapabilityService;
-import org.kainos.ea.api.JobRoleService;
-import org.kainos.ea.cli.RequestCapability;
 import org.kainos.ea.client.FailedToGetCapabilitiesException;
-import org.kainos.ea.client.FailedToGetJobRolesException;
 import org.kainos.ea.db.CapabilityDao;
-import org.kainos.ea.db.JobRoleDao;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-import java.sql.SQLException;
 import java.util.logging.Logger;
 
 @Api("Capability API")
